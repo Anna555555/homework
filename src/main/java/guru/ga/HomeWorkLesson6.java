d112be2318614b5733d2eeb73dcf3eec43fa1197
Anna555555 <ilinaanna858@gmail.com>
2023-11-18 15:10:01 +0300

    hw done

---
+++ src/main/java/guru/ga/HomeWorkLesson6.java
@@ -0,0 +1,19 @@
+package guru.ga;
+
+public class HomeWorkLesson6 {
+
+public static void main(String[]args){
+
+    byte aByte = 100;
+    byte fByte = 101;
+    System.out.println("aByte+fByte"); //переполнение
+
+    int a = 15;
+    int b = 7;
+    System.out.println("Сумма: a + b = " + (a + b));
+    System.out.println("Разность: a - b = " + (a - b));
+    System.out.println("Умножение: a * b = " + (a * b));
+    System.out.println("Деление: a / b = " + (a / b));
+    System.out.println("Остаток от деления: a % b = " + (a % b));
+}
+}
