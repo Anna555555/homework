a51418b2d91ae16e3c6b7a0d20f23bae2d7d5939
Anna555555 <ilinaanna858@gmail.com>
2023-11-18 19:54:02 +0300

    hw done

--- src/test/java/HomeWorkLesson4.java
+++ src/test/java/HomeWorkLesson4.java
@@ -17,9 +17,13 @@ import static com.codeborne.selenide.Selenide.open;
         //Перейдите в раздел Wiki проекта
         $(".js-wiki-more-pages-link").click();
         $(byText("SoftAssertions")).click();
+        $(".gh-header-title").shouldHave(text("SoftAssertions"));
                // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
                 //Откройте страницу SoftAssertions
-        $("#user-content-3-using-junit5-extend-test-class").sibling(0).$("pre").shouldHave(text("@Test"));
+        $("#user-content-3-using-junit5-extend-test-class").parent().sibling(0).$("pre").shouldHave(text("@Test"));
         } //проверьте что внутри SoftAssertions есть пример кода для JUnit5
 
         }
+/*<span data-view-component="true" class="Truncate">
+<a href="/selenide/selenide/wiki/SoftAssertions" data-view-component="true" class="Truncate-text text-bold py-1">SoftAssertions</a>
+</span> */
